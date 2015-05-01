076cd16dcf2a71e5e0507893b1c6b790e4cbe2fc
g.rossetti <g.rossetti@loccioni.com>
2015-05-01 11:51:40 +0200

    WebAPI Project

---
+++ oauth2pill/ProtectedResource2/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ProtectedResource2.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
